113cb2a700f4db23c45938a2e207096e36209608
Suyash Mohan <suyashatweb@gmail.com>
2014-04-15 23:29:50 +0530

    Enemies are Bad

--- game.js
+++ game.js
@@ -18,76 +18,131 @@
 		},
 
 		create : function(){
+			this.lastBullet = 0;
+			this.lastEnemy = 0;
+			this.lastTick = 0;
+			this.speed = 100;
+			this.enemySpeed = 200;
+			this.bulletSpeed = 300;
+			this.lives = 3;
+			this.score = 0;
+
+			this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
 			this.bg = this.game.add.tileSprite(0,0,1782,600,'bgSpace');
-			this.bg.autoScroll(-100,0);
+			this.bg.autoScroll(-this.speed,0);
 
 			this.ship = this.game.add.sprite(10,HEIGHT/2, 'ship');
 			this.ship.animations.add('move');
 			this.ship.animations.play('move', 20, true);
+			this.game.physics.arcade.enable(this.ship, Phaser.Physics.ARCADE);
 
 			this.bullets = this.game.add.group();
 			this.bullets.enableBody = true;
 			this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+			this.bullets.createMultiple(10,'bullet');			
+    		this.bullets.setAll('outOfBoundsKill', true);
+    		this.bullets.setAll('checkWorldBounds', true);
+
 
 			this.enemies = this.game.add.group();
 			this.enemies.enableBody = true;
 			this.enemies.physicsBodyType = Phaser.Physics.ARCADE;
-
-			this.lastBullet = 0;
-			this.lastEnemy = 0;
-			this.speed = 2;
-			this.enemySpeed = -150;
-			this.bulletSpeed = 300;
 		},
 
 		update : function(){
-			var curTime = this.game.time.now;
-
+			this.ship.body.velocity.setTo(0,0);
 			if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && this.ship.x > 0)
 			{
-				this.ship.x -= this.speed;
+				this.ship.body.velocity.x = -2*this.speed;
 			}
 			else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && this.ship.x < (WIDTH-this.ship.width))
 			{
-				this.ship.x += this.speed;
+				this.ship.body.velocity.x = this.speed;
 			}
 			else if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP) && this.ship.y > 0)
 			{
-				this.ship.y -= this.speed;
+				this.ship.body.velocity.y = -this.speed;
 			}
 			else if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN) && this.ship.y < (HEIGHT-this.ship.height))
 			{
-				this.ship.y += this.speed;
+				this.ship.body.velocity.y = +this.speed;
 			}
+
+			var curTime = this.game.time.now;
 
 			if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
 			{
 				if(curTime - this.lastBullet > 300)
 				{
-					var bullet = this.bullets.create(this.ship.x + this.ship.width,this.ship.y+this.ship.height/2,'bullet');
-					bullet.body.velocity.x = this.bulletSpeed;
-					bullet.checkWorldBounds = true;
-					var that = this;
-					bullet.events.onOutOfBounds.add(function(obj){
-						that.bullets.remove(obj);
-					},this);
+					this.fireBullet();
 					this.lastBullet = curTime;
 				}
 			}
 
-			if(curTime - this.lastEnemy > 1000)
+			if(curTime - this.lastEnemy > 500)
 			{
-				var enemy = this.enemies.create(WIDTH - 30,Math.floor(Math.random()*(HEIGHT-30)),'enemyship'+(1+Math.floor(Math.random()*5)));
-				enemy.body.velocity.x = this.enemySpeed;
-				enemy.checkWorldBounds = true;
-				enemy.animations.add('move');
-				enemy.animations.play('move',20,true);
-				var that = this;
-				enemy.events.onOutOfBounds.add(function(obj){
-					that.enemies.remove(obj);
-				},this);
+				this.generateEnemy();
 				this.lastEnemy = curTime;
 			}
+
+			if(curTime - this.lastTick > 10000)
+			{
+				if(this.speed < 500)
+				{
+					this.speed *= 1.1;
+					this.enemySpeed *= 1.1;
+					this.bulletSpeed *= 1.1;
+					this.bg.autoScroll(-this.speed, 0);
+					this.lastTick = curTime;
+				}
+			}
+
+			this.game.physics.arcade.collide(this.enemies, this.ship, this.enemyHitPlayer,null, this);
+			this.game.physics.arcade.collide(this.enemies, this.bullets, this.enemyHitBullet,null, this);
+		},
+
+		fireBullet : function(curTime){
+			var bullet = this.bullets.getFirstExists(false);
+			if(bullet)
+			{
+				bullet.reset(this.ship.x+this.ship.width,this.ship.y+this.ship.height/2);
+				bullet.body.velocity.x = this.bulletSpeed;
+			}
+		},
+
+		generateEnemy : function(){
+			var enemy = this.enemies.getFirstExists(false);
+			if(enemy)
+			{
+				enemy.reset(WIDTH - 30,Math.floor(Math.random()*(HEIGHT-30)),'enemyship'+(1+Math.floor(Math.random()*5)));
+			}
+			else
+			{
+				enemy = this.enemies.create(WIDTH - 30,Math.floor(Math.random()*(HEIGHT-30)),'enemyship'+(1+Math.floor(Math.random()*5)));
+			}
+			enemy.body.velocity.x = -this.enemySpeed;
+			enemy.outOfBoundsKill = true;
+			enemy.checkWorldBounds = true;
+			enemy.animations.add('move');
+			enemy.animations.play('move',20,true);
+		},
+
+		enemyHitPlayer : function(player, enemy){
+			if(this.enemies.getIndex(enemy) > -1)
+				this.enemies.remove(enemy);
+			enemy.kill();
+			this.lives -= 1;
+			console.log("Lives Remaining : " + this.lives);
+		},
+
+		enemyHitBullet : function(bullet, enemy){
+			if(this.enemies.getIndex(enemy) > -1)
+				this.enemies.remove(enemy);
+			enemy.kill();
+			bullet.kill();
+			this.score += 10;
+			console.log("Score : " + this.score);
 		}
 	}
 
